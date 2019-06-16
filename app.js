b8c393d30bc7da2671288735eae91b7e66979146
Matthew Tran <matthewtran@matthews-air.lan>
2019-06-16 08:42:14 -0700

    set up calculation

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-$("p").css("color", "red");
+$("#calculate").click(function () {
+    event.preventDefault();
+    let weight = $('#weightvalue').val();
+    let conversionFactor = $('#planet option:selected').val();
+    let newWeight = weight * conversionFactor;
+    let planet = $('#planet option:selected').text();
+    let weightMessage = 'Your weight on ' + planet + ' would be ' + newWeight +  ' pounds';
+    $('#calculatedWeight').text(weightMessage);
+});
