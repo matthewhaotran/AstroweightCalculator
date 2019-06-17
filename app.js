eb59b9a61566219441e89d894252bd38cc4a89b4
Matthew Tran <matthewtran@matthews-air.lan>
2019-06-16 22:41:50 -0700

    input validation

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ $("#calculate").click(function () {
     event.preventDefault();
     
     //Collect weight value
-    let weight = $('#weightvalue').val();
+    let weight = Number($('#weightvalue').val());
 
     //Validation checks
-    if (typeof weight === 'string') {
+    if (isNaN(weight)) {
         $('#warning').css("display", "block").text('Please enter a valid weight value');
+        $('#calculatedWeight').css("display", "none")
         return;
     }
     if (weight < 0) {
         $('#warning').css("display", "block").text('The weight cannot be less than 0');
+        $('#calculatedWeight').css("display", "none")
         return;
     }
     if (weight > 392) {
         $('#warning').css("display", "block").text('Please enter a weight less than 392 pounds.');
+        $('#calculatedWeight').css("display", "none")
         return;
     }
 
     //Calculate weight
+    $('#warning').css("display", "none")
     let conversionFactor = $('#planet option:selected').val();
     let newWeight = weight * conversionFactor;
     let planet = $('#planet option:selected').text();
