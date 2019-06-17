62afc5485e0ef01df953d190ac2fdc9b27505617
Matthew Tran <matthewtran@matthews-air.lan>
2019-06-16 21:35:21 -0700

    check if value is string, negative, or too large

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 $("#calculate").click(function () {
+    //Prevent form from submitting and refreshing page
     event.preventDefault();
+    
+    //Collect weight value
     let weight = $('#weightvalue').val();
+
+    //Validation checks
+    if (typeof weight === 'string') {
+        $('#warning').css("display", "block").text('Please enter a valid weight value');
+        return;
+    }
+    if (weight < 0) {
+        $('#warning').css("display", "block").text('The weight cannot be less than 0');
+        return;
+    }
+    if (weight > 392) {
+        $('#warning').css("display", "block").text('Please enter a weight less than 392 pounds.');
+        return;
+    }
+
+    //Calculate weight
     let conversionFactor = $('#planet option:selected').val();
     let newWeight = weight * conversionFactor;
     let planet = $('#planet option:selected').text();
